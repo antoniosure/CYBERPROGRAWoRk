d048df163a6181cb4712f37a7a972ef0e00de810
abrahamjapg <javier.palomaresg@uanl.edu.mx>
2024-09-12 19:58:01 -0600

    Add files via upload

---
+++ MenuPIA.ps1
@@ -0,0 +1,27 @@
+﻿$repetir = Get-Service
+foreach($reg in $repetir){
+$menu = "Menu
+1)Revision de hashes y consulta de API
+2)Visualizar archivos ocultos 
+3)Revision de uso de recursos del sistema
+4)¿¿Tarea adicional??
+5)Salir
+Ingresa un numero [1-5]: "
+$opc = Read-Host -Prompt $menu
+switch ($opc) {
+      1{
+        $variable1 = Read-Host -Prompt "Ingresa que archivo hash deseas visualizar bajo los archivos locales"
+    } 2{
+        $variable2 = Read-Host -Prompt "Ingresa el listado de archivos ocultos que desea ver"
+    } 3{
+        Read-Host "Ingresa que recurso deseas ver"
+    } 4{
+        Read-Host "¿¿Tarea adicional??"
+    } 5{
+        Write-Host "Adios"
+        break foreach
+    } default {
+    Write-Host "Es necesario elegir una opcion"
+    }
+}
+}
