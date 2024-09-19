86c815aaa16035bf1d5009ecfec2770bfe70d212
eli-ramirez <montserrat.ramirezc@uanl.edu.mx>
2024-09-18 22:30:07 -0600

    Add files via upload

---
+++ Modulo2_HiddenFiles.psm1
@@ -0,0 +1,40 @@
+﻿function HiddenFiles {
+    <#
+    .SYNOPSIS
+    Muestra una lista de archivos ocultos en una carpeta específica.
+
+    .DESCRIPTION
+    Esta función permite al usuario especificar una ruta y listará todos los archivos ocultos
+    encontrados en esa ruta. Si la carpeta no existe o no hay archivos ocultos, se informará
+    al usuario.
+
+    .PARAMETER ruta
+    Ruta de la carpeta donde se buscarán archivos ocultos. Este parámetro es obligatorio.
+
+    .EXAMPLE
+    HiddenFiles -ruta "C:\MiCarpeta"
+    Este ejemplo buscará y mostrará todos los archivos ocultos en "C:\MiCarpeta".
+
+    .NOTES
+    Autor: Montserrat Elizeth Ramírez Cuéllar
+    Versión: 01
+    #>
+
+    param (
+        [parameter(Mandatory)][string]$ruta
+    )
+    
+    if (-not (Test-Path -Path $ruta)) {
+        Write-Host "La carpeta especificada no existe."
+        return
+    }
+
+    $archivosOcultos = Get-ChildItem -Path $ruta -Force | Where-Object { $_.Attributes -match "Hidden" }
+                
+    if ($archivosOcultos.Count -eq 0) {
+        Write-Host "No se encontraron archivos ocultos."
+    } else {
+        Write-Host "Listado de archivos ocultos:"
+        $archivosOcultos | ForEach-Object { Write-Host $_.FullName }
+    }
+}
